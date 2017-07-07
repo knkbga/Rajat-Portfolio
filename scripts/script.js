29d7d2a3667207d02b824479b6a7b3d2005c8815
Kanak <kanakbagga1@gmail.com>
2017-07-07 17:04:57 +0530

    Glichtes in ABOUT ME fixed...

--- scripts/script.js
+++ scripts/script.js
@@ -176,6 +176,23 @@ $(function(){
 			$('#portfolio').fadeIn();
 		});
 	}
+    
+    $(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function(event){
+            var xMove = event.originalEvent.touches[0].pageX;
+            if( Math.floor(xClick - xMove) > 5 ){
+                $(".carousel").carousel('next');
+            }
+            else if( Math.floor(xClick - xMove) < -5 ){
+                $(".carousel").carousel('prev');
+            }
+        });
+        $(".carousel").on("touchend", function(){
+                $(this).off("touchmove");
+        });
+    });
+    
 });
 
 // becoz couldn't be called inside document.ready function (an onclick event)
